734e6003d6d408c65f4f912a0d308697a7254b0d
Dan Ngo <danngo.au@gmail.com>
2025-10-10 18:15:19 +1100

    set up .gitignore

---
+++ Wed-16.30-MD-G2-Server/Controllers/HelloController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wed_16._30_MD_G2_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+    }
+}
