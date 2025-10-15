3fc94673560becbb203bc7eef923259fc7d31fd3
Dan Ngo <danngo.au@gmail.com>
2025-10-15 14:24:47 +1100

    Math controller with sum endpoint

---
+++ Wed-16.30-MD-G2-Server/Controllers/MathController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Wed_16._30_MD_G2_Server.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class MathController : ControllerBase
+{
+    [HttpGet("sum")]
+    public IActionResult Get([Required] int firstNum, 
+                             [Required] int secondNum)
+    {
+        return Ok(firstNum + secondNum);
+    }
+}
