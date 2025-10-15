16ce54dc52fb4759da68f5ef5a4c811cce499f43
Dan Ngo <danngo.au@gmail.com>
2025-10-15 16:07:14 +1100

    Hello controller test

---
+++ ApiTests/HelloControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Wed_16._30_MD_G2_Server.Controllers;
+
+namespace ApiTests;
+
+[TestClass]
+public sealed class HelloControllerTests
+{
+
+    [TestMethod]
+    [DataRow("Development")]
+    [DataRow("Staging")]
+    [DataRow("Production")]
+    public void TestSayHello(string envName)
+    {
+        var env = new Mock<IWebHostEnvironment>();
+        env.SetupGet(e => e.EnvironmentName)
+           .Returns(envName);
+
+        var controller = new HelloController(env.Object);
+
+        var result = controller.Get();
+        var obj = result.Result as ObjectResult;
+
+        Assert.AreEqual(200, obj?.StatusCode ?? 500);
+
+        var message = obj?.Value as string;
+        Assert.IsNotNull(message);
+        Assert.AreEqual($"Hello from {envName} environment!", message);
+
+        /*Assert.Contains(envName, message);
+        Assert.StartsWith("Hello from ", message);
+        Assert.EndsWith(" environment!", message);*/
+    }
+}
