c7d72f54d0325d3e4c69affb8b40f549f35aeec2
Dan Ngo <danngo.au@gmail.com>
2025-10-19 15:25:36 +1100

    proper test file

---
+++ ApiTests/MathControllerTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Wed_16._30_MD_G2_Server.Controllers;
+
+namespace ApiTests;
+
+[TestClass]
+public sealed class MathControllerTests
+{
+    [TestMethod]
+    [DataRow(1, 1, 2)]
+    [DataRow(5, 7, 12)]
+    [DataRow(10, 20, 30)]
+    public void TestSumCalculation(int num1, int num2, int expectedResult)
+    {
+        var controller = new MathController();
+
+        var result = controller.Get(num1, num2);
+        var obj = result.Result as ObjectResult;
+
+        Assert.AreEqual(200, obj?.StatusCode ?? 500);
+        Assert.IsNotNull(obj?.Value);
+        Assert.AreEqual(expectedResult, obj?.Value);
+    }
+}
